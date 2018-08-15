8dd86d894c23c4a21aa789a3450449c5a8d4fd5a
Matheus Maydana <matheus@objetivawebsites.com.br>
2018-08-15 13:02:09 -0300

    15/08/2018 - Estrutura básica - MVC
    -

---
+++ Model/Bancodados/Pssw.php
@@ -0,0 +1,7 @@
+<? 
+
+define('DB_HOST', '127.0.0.1');    // Nome Host
+define('DB_PORT', '5432');     // port banco dados (pgsql)
+define('DB_NAME', 'matheusmaydana');    // Nome Banco
+define('DB_USER', 'matheus');    // Usuário banco
+define('DB_PASS', '123456');    // Senha Usuário
